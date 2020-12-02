edefe25f97f4e3fa2e1c91d429e82df1a71df0a6
Kim Leibowitz <kim@brightlightwellness.com>
2020-12-02 09:23:31 -0500

    working on filtering books in three lists (failed to compile), added delete function

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -19,28 +19,78 @@ const AllBooks = ({ auth }) => {
     return <Redirect to='/login' />
   }
 
+  const readBooks = books.filter(book => book.status === 'read')
+  const toReadBooks = books.filter(book => book.status === 'toread')
+  const readingBooks = books.filter(book => book.status === 'reading')
+
+  function deleteBook (bookToDelete) {
+    axios.delete(`https://books-api.glitch.me/api/books/${bookToDelete._id}`, {
+      auth: auth
+    })
+      .then(res => {
+        // Remove the book that we just deleted
+        // from our books array in state.
+        setBooks(books.filter(currentBook => (
+          currentBook._id !== bookToDelete._id)
+        ))
+      })
+  }
+
   return (
     <div className='BookList'>
       <h1 className='mh2 mv3'>Book List</h1>
       <div className='key flex'>
-        <div> <h3 className='reading ma3 pv4 pa3 ph6'>Reading</h3> </div>
-        <div> <h3 className='toread ma3 pv4 pa3 ph6'>To Read</h3> </div>
-        <div> <h3 className='read ma3 pv4 pa3 ph6'>Read</h3> </div>
-      </div>
-      {books.map(book => (
-        <div
+
+        <div> <h3 className='reading ma3 pv4 pa3 ph6'>Reading</h3> 
+        { readingBooks.map(book => (
+         <div 
           key={book._id} className={clsx('ma2 book', {
             reading: book.status === 'reading',
             toread: book.status === 'toread',
             read: book.status === 'read'
           })}
-        >
-          <h2 className='ma2 underline'>{book.title || 'No Title'}</h2>
-          <p className='ma3 i'>Written by {book.authors}</p>
+          >
+            <h2 className='ma2 underline'>{book.title || 'No Title'}</h2>
+            <p className='ma3 i'>Written by {book.authors}</p>
+            <p><button onClick={() => deleteBook(book)}>Delete book</button></p>
+            </div>
+        ))}
+       
+
+        <div> <h3 className='toread ma3 pv4 pa3 ph6'>To Read</h3> 
+          { toReadBooks.map(book => (
+            <div 
+            key={book._id} className={clsx('ma2 book', {
+              reading: book.status === 'reading',
+              toread: book.status === 'toread',
+              read: book.status === 'read'
+            })}
+            >
+            <h2 className='ma2 underline'>{book.title || 'No Title'}</h2>
+            <p className='ma3 i'>Written by {book.authors}</p>
+            <p><button onClick={() => deleteBook(book)}>Delete book</button></p>
+          </div> 
+          ))}
         </div>
-      ))}
-    </div>
-  )
-}
+
+        <div> <h3 className='read ma3 pv4 pa3 ph6'>Read</h3>
+          {readBooks.map(book => (
+            <div
+              key={book._id} className={clsx('ma2 book', {
+                reading: book.status === 'reading',
+                toread: book.status === 'toread',
+                read: book.status === 'read'
+              })}
+            >
+              <h2 className='ma2 underline'>{book.title || 'No Title'}</h2>
+              <p className='ma3 i'>Written by {book.authors}</p>
+              <p><button onClick={() => deleteBook(book)}>Delete book</button></p>
+         </div>
+          ))}
+        </div>
+      </div>
+      
+    )
+} 
 
 export default AllBooks
